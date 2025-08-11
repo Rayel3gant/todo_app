2bbabfe42a210ae7d71c18464b800aa0de144769
Rayel3gant <helloakash04@gmail.com>
2025-08-11 08:51:55 +0530

    fix:add type to use-app-selector

--- hooks/useAppDispatch.ts
+++ hooks/useAppDispatch.ts
@@ -3,6 +3,7 @@ import { useContext } from "react";
 
 export function useAppDispatch() {
   const context = useContext(TodoContext);
-  if (!context) throw new Error("useAppDispatch must be used within StateProvider");
+  if (!context)
+    throw new Error("useAppDispatch must be used within StateProvider");
   return context.dispatch;
 }
--- hooks/useAppSelector.ts
+++ hooks/useAppSelector.ts
@@ -2,10 +2,13 @@ import { StateType } from "@/lib/types";
 import { TodoContext } from "@/services/context/TodoContext";
 import { useContext, useMemo } from "react";
 
-export function useAppSelector<T>(selector: (state: StateType) => T): T {
+export function useAppSelector<Selected>(
+  selector: (state: StateType) => Selected
+): Selected {
   const context = useContext(TodoContext);
-  if (!context) throw new Error("useAppSelector must be used within StateProvider");
+  if (!context) {
+    throw new Error("useAppSelector must be used within StateProvider");
+  }
 
-  // Memoize the derived value
   return useMemo(() => selector(context.state), [context.state, selector]);
 }
