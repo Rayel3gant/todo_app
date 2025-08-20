6b3d80bcf224d4e8325c529ad93c6c9dd9923d1a
Rayel3gant <helloakash04@gmail.com>
2025-08-20 08:53:50 +0530

    chore: tanstack query setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { UserProvider } from "@/lib/context/userContext";
+import ReactQueryProvider from "@/services/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <UserProvider>
-          <Navbar />
-          {children}
-        </UserProvider>
+        <ReactQueryProvider>
+          <UserProvider>
+            <Navbar />
+            {children}
+          </UserProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
